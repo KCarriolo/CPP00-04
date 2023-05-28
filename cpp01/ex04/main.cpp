7c71e42ac3aa2935bba935f15be23abd3320372b
no one cares <kevinfcdmelo@hotmail.com>
2023-05-28 15:15:04 -0300

    Updated

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,11 @@
+#include <>
+
+int	main(int argc, char **agv)
+{
+	if (argc != 4){
+		std::cout << "Invalid arguments!" << std::endl;
+		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
+		return (1);
+	}
+	
+}
