88913c32a310c2b811ff1974f9b2dc9ab5725b9f
Kevin Carriolo <kefernan@c1t3s5.42.rio>
2023-05-30 19:45:53 -0300

    Updated

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,11 +1,23 @@
-#include <>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-int	main(int argc, char **agv)
+int	main(int argc, char **argv)
 {
-	if (argc != 4){
+	/*if (argc != 4){
 		std::cout << "Invalid arguments!" << std::endl;
 		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
 		return (1);
+	}*/
+	std::ifstream file (argv[1]);
+	if (!file.is_open()){
+		std::cout << "Error: file does not exist" << std::endl;
+		return (0);
 	}
-	
+	std::string	arg = argv[1].c_str();
+	std::string	str = "touch ";
+	str = str + argv[1] + ".replace";
+	system(str);
+	//file.open(argv[1], std::ifstream::in)
+	return (1);
 }
