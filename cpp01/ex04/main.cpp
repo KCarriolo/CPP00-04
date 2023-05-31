541f7fa185f2f75c5d19f59c0c392a8dadc734db
Kevin Carriolo <kefernan@c1t3s5.42.rio>
2023-05-31 15:35:52 -0300

    Updated

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 
 int	main(int argc, char **argv)
 {
-	/*if (argc != 4){
+	if (argc != 4){
 		std::cout << "Invalid arguments!" << std::endl;
 		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
 		return (1);
-	}*/
+	}	
+	std::string	arg2 (argv[2]);
+	std::string	arg3 (argv[3]);
+	if (arg2.empty() || arg3.empty()){
+		std::cout << "nope" << std::endl;		
+		return (1);
+	}
 	std::ifstream file (argv[1]);
 	if (!file.is_open()){
 		std::cout << "Error: file does not exist" << std::endl;
-		return (0);
+		return (1);
 	}
-	std::string	arg = argv[1].c_str();
-	std::string	str = "touch ";
-	str = str + argv[1] + ".replace";
-	system(str);
-	//file.open(argv[1], std::ifstream::in)
-	return (1);
+	size_t			pos;
+	std::string		aux;	
+	std::string		arg1 (argv[1]);
+	std::ifstream	newFile(argv[1]);
+	std::ofstream 	outFile (arg1.append(".replace"));
+	//outFile.open(newFile, std::ofstream::out);
+	while (newFile.good() && !newFile.eof() && outFile.good())
+	{
+		getline(newFile, aux);
+		if ((pos = aux.find(arg2,  0)) != std::string::npos){
+				
+		}
+	}
+	return (0);
 }
