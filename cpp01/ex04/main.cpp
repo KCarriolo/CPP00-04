394e2eb4580ef79d186f4fb480f5f65210b019b7
no one cares <kevinfcdmelo@hotmail.com>
2023-06-01 10:32:34 -0300

    Updated

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 int	main(int argc, char **argv)
 {
 	if (argc != 4){
-		std::cout << "Invalid arguments!" << std::endl;
+		std::cout << "Error: Invalid arguments." << std::endl;
 		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
 		return (1);
 	}	
 	std::string	arg2 (argv[2]);
 	std::string	arg3 (argv[3]);
 	if (arg2.empty() || arg3.empty()){
-		std::cout << "Parameters can't be empty!" << std::endl;		
+		std::cout << "Error: Empty parameters." << std::endl;		
 		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
 		return (1);
 	}
 	std::ifstream file (argv[1]);
 	if (!file.is_open()){
-		std::cout << "Error: file does not exist" << std::endl;
+		std::cout << "Error: file does not exist or you don't have permission to access it" << std::endl;
+		file.close();
 		return (1);
 	}
-	if (file.eof()){
-		std::cout << "File is empty" << std::endl;
+	char	c;
+	if (file.get(c) == 0){
+		std::cout << "Error: nothing to be searched and replaced. File is empty." << std::endl;
+		file.close();
 		return (1);
 	}
 	size_t			pos;
@@ -30,6 +32,13 @@ int	main(int argc, char **argv)
 	std::string		arg1 (argv[1]);
 	std::ifstream	auxSwap;
 	std::ofstream 	outFile (arg1.append(".replace").data());
+	if (!outFile.is_open()){
+		std::cout << "Error: You don't have the required permissions for .replace file" << std::endl;
+		file.close();
+		auxSwap.close();
+		outFile.close();
+		return (1);
+	}
 	while (file.good() && !file.eof())
 	{
 		getline(file, aux);
@@ -39,5 +48,8 @@ int	main(int argc, char **argv)
 		}
 		outFile << aux << std::endl;
 	}
+	file.close();
+	auxSwap.close();
+	outFile.close();
 	return (0);
 }
