94b2ca455aef44779e3969a68a9e022e993ac26e
no one cares <kevinfcdmelo@hotmail.com>
2023-05-31 22:46:44 -0300

    Updated

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int	main(int argc, char **argv)
 {
@@ -11,7 +12,8 @@ int	main(int argc, char **argv)
 	std::string	arg2 (argv[2]);
 	std::string	arg3 (argv[3]);
 	if (arg2.empty() || arg3.empty()){
-		std::cout << "nope" << std::endl;		
+		std::cout << "Parameters can't be empty!" << std::endl;		
+		std::cout << "Usage: ./snr filename s1 s2" << std::endl;
 		return (1);
 	}
 	std::ifstream file (argv[1]);
@@ -19,18 +21,23 @@ int	main(int argc, char **argv)
 		std::cout << "Error: file does not exist" << std::endl;
 		return (1);
 	}
+	if (file.eof()){
+		std::cout << "File is empty" << std::endl;
+		return (1);
+	}
 	size_t			pos;
 	std::string		aux;	
 	std::string		arg1 (argv[1]);
-	std::ifstream	newFile(argv[1]);
-	std::ofstream 	outFile (arg1.append(".replace"));
-	//outFile.open(newFile, std::ofstream::out);
-	while (newFile.good() && !newFile.eof() && outFile.good())
+	std::ifstream	auxSwap;
+	std::ofstream 	outFile (arg1.append(".replace").data());
+	while (file.good() && !file.eof())
 	{
-		getline(newFile, aux);
-		if ((pos = aux.find(arg2,  0)) != std::string::npos){
-				
+		getline(file, aux);
+		while ((pos = aux.find(arg2,  0)) != std::string::npos){
+			aux = aux.erase(pos, arg2.length());	
+			aux.insert(pos, arg3);	
 		}
+		outFile << aux << std::endl;
 	}
 	return (0);
 }
