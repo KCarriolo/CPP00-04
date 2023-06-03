a4061a0568601f7a1896dc7f532902ccbbce21e1
no one cares <kevinfcdmelo@hotmail.com>
2023-06-03 01:56:44 -0300

    Updated

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,17 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): fixedValue(0){}
+
+Fixed::Fixed(const &Fixed copy){}
+
+Fixed& Fixed::operator=(const Fixed& obj){}
+
+Fixed::~Fixed(void){}
+
+int	Fixed::getRawBits(void) const{
+	
+}
+
+void	Fixed::setRawBits(int const raw){
+	
+}
---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <string>
+
+class	Fixed{
+	public:
+		Fixed(void);	
+		Fixed(const &Fixed copy);	
+		Fixed& operator=(const Fixed& obj);
+		~Fixed(void);	
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+	private:
+		int					fixedValue;
+		static const int	fractBits;
+};
