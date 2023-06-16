755eb97102606aa7827c20588a7d1f24ab2505af
no one cares <kevinfcdmelo@hotmail.com>
2023-06-15 21:05:41 -0300

    Updated

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -5,9 +5,13 @@
 class	AMateria{
 	protected:
 
-
 	public:
 		AMateria(std::string const & type);
 
 		std::string const & getType() const; //Return the materia type
+
+		virtual AMateria*	clone() const = 0;
+		virtual void	use(ICharacter& target);
 };
+
+#endif
---
+++ cpp04/ex03/Ice.hpp
@@ -0,0 +1,9 @@
+#ifndef ICE_HPP
+# define ICE_HPP
+# include "AMateria.hpp"
+
+class	Ice{
+
+};
+
+#endif
